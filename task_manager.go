01941a8e122b5089b3cd5a169abc574ba28e56f8
sayoru <altayef2008@gmail.com>
2026-09-01 12:55:12 +0400

    Add file persistence: save/load tasks from tasks.txt

--- task_manager.go
+++ task_manager.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
 )
 
 type Task struct {
@@ -64,10 +70,98 @@ func deleteTask(tasks []Task) ([]Task, error) {
 	return tasks, nil
 }
 
-func main() {
+func saveTask(tasks []Task, file *os.File) error {
+	_, err := file.Seek(0, 0)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	err = file.Truncate(0)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	writer := bufio.NewWriter(file)
+
+	for _, t := range tasks {
+		_, err := fmt.Fprintf(writer, "%d|%s|%s|%t\n", t.ID, t.Title, t.Description, t.Completed)
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+	}
+	return writer.Flush()
+}
+
+func loadTask(file *os.File) ([]Task, error) {
+	reader := bufio.NewReader(file)
+
+	var tasks []Task
 
 	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				if line == "" {
+					break
+				}
+			} else {
+				fmt.Println(err)
+				return tasks, err
+			}
+		}
+		line = strings.TrimSpace(line)
+		if line == "" {
+			if err == io.EOF {
+				break
+			}
+			continue
+		}
 
+		parts := strings.Split(line, "|")
+
+		id, err := strconv.Atoi(parts[0])
+		if err != nil {
+			fmt.Println(err)
+			return tasks, err
+		}
+
+		completed, err := strconv.ParseBool(parts[3])
+		if err != nil {
+			fmt.Println(err)
+			return tasks, err
+		}
+
+		t := Task{id, parts[1], parts[2], completed}
+		tasks = append(tasks, t)
+		if err == io.EOF {
+			break
+		}
+	}
+	return tasks, nil
+}
+
+func main() {
+
+	home, err := os.UserHomeDir() //iniatilization of home directory variable
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	path := filepath.Join(home, "Documents", "tasks.txt") //iniatilization of path variable
+
+	file, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0644) //file creation
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	tasks, err = loadTask(file)
+
+	for {
 		fmt.Println(`--- TASK MANAGER ---
 1. Show Task
 2. Add Task
@@ -95,10 +189,32 @@ Choose action:`)
 			fmt.Println("Description:")
 			fmt.Scan(&description)
 			tasks = addTask(tasks, title, description)
+			err = saveTask(tasks, file)
+			if err != nil {
+				fmt.Println(err)
+			}
 		case 3:
-			completeTask(tasks)
+			_, err = completeTask(tasks)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			err = saveTask(tasks, file)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
 		case 4:
-			tasks, _ = deleteTask(tasks)
+			tasks, err = deleteTask(tasks)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			err = saveTask(tasks, file)
+			if err != nil {
+				fmt.Println(err)
+			}
 		case 5:
 			return
 		default:
